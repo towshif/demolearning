7a2db90d3d952c1735664759da6e59b4187e5876
towshif <towshif@gmail.com>
2016-11-08 00:16:42 -0800

    towshifsave2

--- demolearning/main.c
+++ demolearning/main.c
@@ -2,14 +2,15 @@
 //#include "longestword.h"
 //#include "syllable.h"
 //#include "syllCount.h"
-//#include "palindrome.h"
-#include "palindromeAdvanced.h"
+#include "palindrome.h"
+//#include "palindromeAdvanced.h"
+
 void main (){
     
     int d, u;
     printf ("This is the main code\n");
     //syllable();
     //syllCount();
-    //palindrome();
-    palindromeAdvanced();
+    palindrome();
+    //palindromeAdvanced();
 }
